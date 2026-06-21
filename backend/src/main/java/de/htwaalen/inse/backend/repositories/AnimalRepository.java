cabfcd411497a8c48d8bdadb34cd71fdbaa98bbd
adrianhsaalen <adrian.schaedel@studmail.htw-aalen.de>
2026-06-21 16:22:21 +0200

    Update AnimalRepository.java

--- backend/src/main/java/de/htwaalen/inse/backend/repositories/AnimalRepository.java
+++ backend/src/main/java/de/htwaalen/inse/backend/repositories/AnimalRepository.java
@@ -1,13 +1,13 @@
 package de.htwaalen.inse.backend.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import de.htwaalen.inse.backend.entities.Animal;
+
 /*
   Repository für die Tiere (Animal).
   Speichert und lädt die beobachteten Tiere aus der Datenbank.
   Die Methoden zum Speichern, Laden und Löschen kommen von JpaRepository.
  */
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import de.htwaalen.inse.backend.entities.Animal;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
